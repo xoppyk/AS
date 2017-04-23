12211379f1816b540405dc127dc5c12c622123ec
Nikita <xoppyksol3@gmail.com>
2017-04-23 23:13:58 +0100

    Mais Algumas implementacoes na Monitorizacao

--- monitoring.pl
+++ monitoring.pl
@@ -3,49 +3,64 @@
 use warnings;
 use strict;
 
-my $count;
+#Em caso de Erro perl_utf-8
+#export LANGUAGE=en_US.UTF-8
+#export LC_ALL=en_US.UTF-8
+#export LANG=en_US.UTF-8
+#export LC_TYPE=en_US.UTF-8
+
 #CPU
 my @cpu_mhz = execute("cat /proc/cpuinfo | grep -i mhz | cut -d: -f2");
-#TEMP
-my @core_temp = execute("sensors | grep Core | cut -d: -f2 | cut -d'(' -f1 |tr ' ' '#' | tr '#' ' '");
+
 #Procesos Mais Comeloes
 my @top5_cpu = execute("ps -eo\"%p %C %G %c\" --sort %cpu | tail -n 5 | tac");
 
-cpu_view(\@cpu_mhz);
-core_temp_view(\@core_temp);
+#Memory
+my @mem_info = execute("free -h");
+
+#File_System
+my @file_system = execute("df -h| grep 'Filesystem\\|/dev/sd*'");
+
+cpu_freq_view(\@cpu_mhz);
 top5_cpu_view(\@top5_cpu);
+mem_info_view(\@mem_info);
+file_system_view(\@file_system);
+
+#FILE_SYSTEM_VIEW
+sub file_system_view {
+    print "\nStorage\n";
+    my (@file_system) = @{$_[0]};
+    foreach (@file_system) {
+        print "\t$_\n";
+    }
+}
+
+#MEM_INFO_VIEW
+sub mem_info_view {
+    print "\nMem Info\n\t\t";
+    my (@mem_info) = @{$_[0]};
+    foreach (@mem_info) {
+        print "\t$_\n";
+    }
+}
+
 
 #TOP5_CPU_VIEW
 sub top5_cpu_view {
     my (@top5_cpu) = @{$_[0]};
-    print "Top 5 by CPU\n";
+    print "\nTop 5 by CPU\n";
     foreach my $process (@top5_cpu) {
         print "\t$process\n";
     }
 }
 
 #CPU_VIEW
-sub cpu_view {
+sub cpu_freq_view {
     my (@cpu_mhz) = @{$_[0]};
     my $count=1;
-    my $cpu_min = trim(`lscpu | grep "CPU min" | cut -d: -f2 | cut -d, -f1`);
-    my $cpu_max = trim(`lscpu | grep "CPU max" | cut -d: -f2 | cut -d, -f1`);
-    my $cpu_usage;
-    printf ("CPU_FREQ [min:%s][max:%s]\n", $cpu_min, $cpu_max);
+    printf ("CPU_FREQ\n");
     foreach my $cpu (@cpu_mhz) {
-        $cpu_usage = sprintf("%d", $cpu * 100 / $cpu_max);
-        print "\t[$count]:cpu_mhz ".$cpu."\t<".$cpu_usage."\%>"."\n";
-        $count++;
-    }
-}
-
-#CORE_TEMPERATURE_VIEW
-sub core_temp_view {
-    my (@core_temp) = @{$_[0]};
-    $count=1;
-    print "CORE_TEMP\n";
-    foreach my $temp (@core_temp) {
-        print "\t[$count]:core_temp ".$temp."\n";
+        print "\t[$count]:cpu_mhz ".$cpu."\n";
         $count++;
     }
 }
