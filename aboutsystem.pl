8abacd38d02812252164698c33214bb433c79cc3
Carolina <anacarolinadias@live.com.pt>
2017-05-04 17:45:40 +0100

    commands v2

--- aboutsystem.pl
+++ aboutsystem.pl
@@ -7,22 +7,28 @@ printf "ABOUT SYSTEM\n";
 my $system="";
 my $uptime="";
 my $arch="";
+my $processor="";
+my $cores="";
+my $processorNumber;
 
 $system = "uname -r -v | tr -s \" \" \":\" | cut -d \":\" -f1,2 | tr -s \":\" \" \" ";
 $uptime = "uptime | tr -s \" \" \",\" | cut -d \",\" -f4";
 $arch = "arch";
+$processor="ps -eo\"%C %G %c\" --sort %cpu | tail -n 4 | tac";
+$cores="cat /proc/cpuinfo | grep 'core id'";
+$processorNumber="ps -eo\"%C %G %c\" --sort %cpu | tail -n 4 | tac | wc -l";
 
-printf "Sistema: "; 
+printf "System: "; 
 system $system;
 printf "Running Time: ";
 system $uptime;
 printf "Architecture: ";
 system $arch; 
 printf "Processor Name: ";
-
+system $processor;
 printf "Cores Number: ";
-
+system $cores;
 printf "Processor Number: ";
-
+system $processorNumber;
 
 exit(0);
