58a1362391fae016288ce14db552a7e7e4f7435c
Silveriodias <anacarolinadias@live.com.pt>
2017-05-11 15:14:46 +0100

    Update aboutsystem.pl
    v2

--- aboutsystem.pl
+++ aboutsystem.pl
@@ -9,21 +9,22 @@ my $uptime="";
 my $arch="";
 my $processor="";
 my $cores="";
-my $processorNumber;
+my $processorNumber="";
 
 $system = "uname -r -v | tr -s \" \" \":\" | cut -d \":\" -f1,2 | tr -s \":\" \" \" ";
 $uptime = "uptime | tr -s \" \" \",\" | cut -d \",\" -f4";
 $arch = "arch";
 $processor="ps -eo\"%C %G %c\" --sort %cpu | tail -n 4 | tac";
-$cores="cat /proc/cpuinfo | grep 'core id'";
+#$cores="cat /proc/cpuinfo | grep 'core id'";
+$cores="cat /proc/cpuinfo | grep 'core id' | tr -s \" \" \"#\" | cut -d \"#\" -f 2 | wc -l";
 $processorNumber="ps -eo\"%C %G %c\" --sort %cpu | tail -n 4 | tac | wc -l";
 
-printf "System: "; 
+printf "System: ";
 system $system;
 printf "Running Time: ";
 system $uptime;
 printf "Architecture: ";
-system $arch; 
+system $arch;
 printf "Processor Name: ";
 system $processor;
 printf "Cores Number: ";
